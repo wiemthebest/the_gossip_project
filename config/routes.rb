4ed42b0ead7dbbd0a5229adc9f30e6362c705169
Wiem Trabelsi <wiem2007@gmail.com>
2021-05-13 00:30:51 +0200

    [2.2.3] create city show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
   resources :gossips do
     resources :comments, only: [:new, :create, :index, :destroy]
   end
+  get 'city/:id', to: 'city#show'
   get 'author/:id', to: 'author#show'
   get 'gossips/:id', to: 'gossips#show'
   #get 'potin/:id', to: 'potin#show'
